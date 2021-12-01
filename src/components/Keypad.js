656e26f405c7fc409e7493dafceef6519a026e77
Christina Nguyen <cnguyen4290@yahoo.com>
2021-11-30 23:12:36 -0600

    Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+
+import React from 'react'
+
+function Keypad() {
+    function renderText() {
+        console.log("Entering password...")
+    }
+    return (
+        <input onChange={renderText} type="password" />
+    )
+}
+
+export default Keypad
